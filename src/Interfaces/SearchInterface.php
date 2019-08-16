535f01211af588db8f9654e434dd6d37c80341f6
Rashmit Singh <rashmit@loyaltycorp.com.au>
2019-08-16 13:49:59 +1000

    Split handler interface into a parent search interface. This can be used by non doctrine search handlers.

---
+++ src/Interfaces/SearchInterface.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace LoyaltyCorp\Search\Interfaces;
+
+interface SearchInterface
+{
+    /**
+     * Returns the index name that this handler is responsible for.
+     *
+     * @return string
+     */
+    public function getIndexName(): string;
+
+    /**
+     * Returns Elasticsearch mappings for index creation.
+     *
+     * @return mixed[]
+     */
+    public static function getMappings(): array;
+
+    /**
+     * Returns Elasticsearch settings for index creation.
+     *
+     * @return mixed[]
+     */
+    public static function getSettings(): array;
+
+    /**
+     * Transforms objects supplied into serialized search arrays that
+     * should be indexed.
+     *
+     * @param mixed|null $object
+     *
+     * @return mixed[][]
+     */
+    public function transform($object = null): ?array;
+}

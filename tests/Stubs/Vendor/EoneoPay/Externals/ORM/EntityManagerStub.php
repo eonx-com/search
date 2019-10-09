353887c38f52767614d2d722f0740a94807230c7
Ketan Padia <ketan@loyaltycorp.com.au>
2019-10-10 09:54:36 +1100

    PYMT-1307 Fix lint errors.

--- tests/Stubs/Vendor/EoneoPay/Externals/ORM/EntityManagerStub.php
+++ tests/Stubs/Vendor/EoneoPay/Externals/ORM/EntityManagerStub.php
@@ -21,7 +21,7 @@ class EntityManagerStub implements EntityManagerInterface
     /**
      * EntityManagerStub constructor.
      *
-     * @param mixed[]|null $entities
+     * @param \EoneoPay\Externals\ORM\Interfaces\EntityInterface[]|null $entities
      */
     public function __construct(?array $entities = null)
     {

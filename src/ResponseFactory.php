2ddc0194123c59edcefbb1b094cf9f0c39761ff1
Tim Nagel <tim@nagel.com.au>
2019-10-30 16:15:44 +1100

    Wrapping the search query with access tokens

--- src/ResponseFactory.php
+++ src/ResponseFactory.php
@@ -5,8 +5,10 @@ namespace LoyaltyCorp\Search;
 
 use EoneoPay\Externals\HttpClient\Interfaces\ClientInterface;
 use LoyaltyCorp\Search\Interfaces\ResponseFactoryInterface;
+use LoyaltyCorp\Search\Interfaces\Access\AccessPopulatorInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
+use function GuzzleHttp\Psr7\stream_for;
 
 final class ResponseFactory implements ResponseFactoryInterface
 {
@@ -28,8 +30,11 @@ final class ResponseFactory implements ResponseFactoryInterface
     /**
      * {@inheritdoc}
      */
-    public function sendRequest(RequestInterface $request): ResponseInterface
-    {
+    public function sendRequest(
+        RequestInterface $request,
+        ?array $accessTokens = null
+    ): ResponseInterface {
+        $request = $this->addAccessControl($request, $accessTokens);
         $response = $this->client->sendRequest($request);
 
         $response = $response
@@ -42,5 +47,54 @@ final class ResponseFactory implements ResponseFactoryInterface
             ->withoutHeader('Access-Control-Max-Age');
 
         return $response;
+    }
+
+    /**
+     * Injects access control related parts into the search request.
+     *
+     * @param RequestInterface $request
+     * @param array $accessTokens
+     *
+     * @return RequestInterface
+     */
+    private function addAccessControl(RequestInterface $request, array $accessTokens): RequestInterface
+    {
+        if ($request->getBody()->isSeekable() === true) {
+            $request->getBody()->rewind();
+        }
+
+        $body = \json_decode(
+            $request->getBody()->getContents(),
+            true,
+            512,
+            \JSON_THROW_ON_ERROR
+        );
+
+        // If the search request doesnt have
+        $query = $body['query'] ?? ['match_all' => []];
+
+        // Exclude the access field from the _source key of results
+        $body['_source'] = [
+            'excludes' => [AccessPopulatorInterface::ACCESS_TOKEN_PROPERTY]
+        ];
+
+        // Wrap the entire query in a bool/filter
+        $body['query'] = [
+            'bool' => [
+                'should' => $query
+            ],
+            'filter' => [
+                [
+                    'term' => [
+                        AccessPopulatorInterface::ACCESS_TOKEN_PROPERTY => $accessTokens ?: ['anonymous']
+                    ]
+                ]
+            ]
+        ];
+
+        $modifiedBody = stream_for(\json_encode($body, \JSON_THROW_ON_ERROR));
+
+        return $request->withMethod('POST')
+            ->withBody($modifiedBody);
     }
 }
